4f259d46e8066497e2f359939095bf80721beb5b
mono <mono0926@gmail.com>
2025-06-25 18:24:10 +0900

    Specify User-Agent

--- lib/src/fetch_link_data_base.dart
+++ lib/src/fetch_link_data_base.dart
@@ -14,7 +14,10 @@ class FetchLinkData {
   static Future<LinkData> fetchLinkData(String url) async {
     final LinkData linkData = LinkData();
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await http.get(Uri.parse(url), headers: {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      });
       if (response.statusCode == 200) {
         final document = parser.parse(
             _encodingForHeaders(response.headers).decode(response.bodyBytes));
